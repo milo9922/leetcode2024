d0b8c8299382b9bf06ccad5bb832ff065db85214
milo9922 <milo99@vp.pl>
2024-03-24 17:24:06 +0100

    LEETCODE-2011 Added final value of variable after performing operations problem solution

---
+++ src/main/java/org/example/arrays/easy/FinalValueOfVariableAfterPerformingOperations_2011.java
@@ -0,0 +1,24 @@
+package org.example.arrays.easy;
+
+/**
+ * Problem description: https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
+ * Status: SOLVED
+ */
+public class FinalValueOfVariableAfterPerformingOperations_2011 {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String operation : operations) {
+            switch (operation) {
+                case "--X":
+                case "X--":
+                    x--;
+                    break;
+                case "X++":
+                case "++X":
+                    x++;
+                    break;
+            }
+        }
+        return x;
+    }
+}
