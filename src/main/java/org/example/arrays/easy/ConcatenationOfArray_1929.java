0188fb2d6b9c41092ac6b3a73e80f8c1bed8ecb8
milo9922 <milo99@vp.pl>
2024-03-24 16:17:57 +0100

    LEETCODE-1929 Added concatenation of array problem solution

---
+++ src/main/java/org/example/arrays/easy/ConcatenationOfArray_1929.java
@@ -0,0 +1,17 @@
+package org.example.arrays.easy;
+
+/**
+ * Problem description: https://leetcode.com/problems/concatenation-of-array/description/
+ * Status: SOLVED
+ */
+
+public class ConcatenationOfArray_1929 {
+    public int[] getConcatenation(int[] nums) {
+        int [] result = new int[nums.length*2];
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = nums[i];
+            result[nums.length + i] = nums[i];
+        }
+        return result;
+    }
+}
