0d6350671b1d6351fb9a12413d81dcba72feaa53
milo9922 <milo99@vp.pl>
2024-03-24 18:49:12 +0100

    LEETCODE-1470 Added shuffle the array problem solution

---
+++ src/main/java/org/example/arrays/easy/ShuffleTheArray_1470.java
@@ -0,0 +1,18 @@
+package org.example.arrays.easy;
+
+/**
+ * Problem description: https://leetcode.com/problems/shuffle-the-array/description/
+ * Status: SOLVED
+ */
+public class ShuffleTheArray_1470 {
+    public int[] shuffle(int[] nums, int n) {
+        int [] result = new int [2*n];
+        int idx = 0;
+        for(int i = 0; i < nums.length; i+=2) {
+            result[i] = nums[idx];
+            result[i+1] = nums[n+idx];
+            idx++;
+        }
+        return result;
+    }
+}
