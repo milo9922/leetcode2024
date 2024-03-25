d552bc3bf4b090e4fcca7877f25c0e2c26495970
milo9922 <milo99@vp.pl>
2024-03-25 21:39:56 +0100

    LEETCODE-2942 Added find words containing character problem solution

---
+++ src/main/java/org/example/arrays/easy/FindWordsContainingCharacter_2942.java
@@ -0,0 +1,23 @@
+package org.example.arrays.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem description: https://leetcode.com/problems/find-words-containing-character/description/
+ * Status: SOLVED
+ */
+public class FindWordsContainingCharacter_2942 {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> indexes = new ArrayList<>();
+        for(int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                if (words[i].charAt(j) == x) {
+                    indexes.add(i);
+                    break;
+                }
+            }
+        }
+        return indexes;
+    }
+}
